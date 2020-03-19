84b0a4a702fd1b1fe9174bc924d6e96fa40b5a0f
The Floating String <laurencel2001@gmail.com>
2020-03-18 22:38:54 -0400

    Create webscraping for Montreal and Quebec cases and create get endpoint

---
+++ src/webscrape_data.py
@@ -0,0 +1,25 @@
+import os
+import requests
+from bs4 import BeautifulSoup
+
+class CovidScraper:
+
+	def __init__(self):
+		self.page = requests.get(os.environ.get("QUEBEC_GOVT_WEBSITE"))
+		self.soup = BeautifulSoup(self.page.content, 'html.parser')
+		self.htmltable = self.soup.find('table', { 'class' : 'contenttable' })
+		self.rows = self.htmltable.find_all("tr")
+		self.montreal_cases = -1
+		self.quebec_prov_cases = -1
+		for row in self.rows:
+			row_list = row.find_all("td")
+			# print(row_list)
+			if len(row_list)>=2:
+				if "Montr" in row_list[0].text:
+					self.montreal_cases = int(row_list[1].text)
+				elif "Total" in row_list[0].text:
+					self.quebec_prov_cases = int(row_list[1].text)
+
+	def return_values(self):
+		return_dict = {"montreal_cases":self.montreal_cases, "quebec_prov_cases":self.quebec_prov_cases}
+		return return_dict
